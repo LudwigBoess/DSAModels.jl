78bba51861014a7647b8e1b9710dd897e48834d4
LudwigBoess <lboess@usm.lmu.de>
2021-11-26 15:04:33 +0100

    implemented magnetic field geometry dependent efficiency

---
+++ src/B_models/dubios.jl
@@ -0,0 +1,46 @@
+const δθ = π / 18
+
+"""
+    η_B(θ_B::T, θ_crit::T) where {T<:Real}
+
+Calculate B angle dependent efficiency component following Dubois+ 2019, eq. 20, http://arxiv.org/abs/1907.04300 
+"""
+@inline function η_B(θ_B::T, θ_crit::T) where {T<:Real}
+    (tanh((θ_crit - θ_B) / δθ) + 1) / 2
+end
+
+"""
+    ηB_acc_e(θ_B::T) where T
+
+Magnetic field geometry dependent efficiency for electrons.
+"""
+function ηB_acc_e(θ_B::T) where {T<:Real}
+    1 - η_B(θ_B, T(π / 4))
+end
+
+"""
+    ηB_acc_p(θ_B::T) where T
+
+Magnetic field geometry dependent efficiency for protons.
+"""
+function ηB_acc_p(θ_B::T) where {T<:Real}
+    η_B(θ_B, T(π / 4))
+end
+
+"""
+    ηB_reacc_e(θ_B::T) where T
+
+Magnetic field geometry dependent efficiency for electrons.
+"""
+function ηB_reacc_e(θ_B::T) where {T<:Real}
+    1 - η_B(θ_B, T(π / 6))
+end
+
+"""
+    ηB_reacc_p(θ_B::T) where T
+
+Magnetic field geometry dependent efficiency for protons.
+"""
+function ηB_reacc_p(θ_B::T) where {T<:Real}
+    η_B(θ_B, T(π / 3))
+end
